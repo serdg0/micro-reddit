e18d572ece0283b96f0ab20617e00473328f9f87
Cristian <inesherald@gmail.com>
2019-08-29 17:57:54 -0500

    fix linter

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,13 +1,6 @@
-# frozen_string_literal: true
-
 class Post < ApplicationRecord
-  validates :title, presence: true,
-                    uniqueness: true
+  validates :title, presence: true, uniqueness: true
   validates :body, presence: true
   belongs_to :user
   has_many :comment
-<<<<<<< HEAD
 end
-=======
-  end
->>>>>>> a434b67d3cc0716f53bc641aa1773d88689e373b
