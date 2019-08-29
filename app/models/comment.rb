959979d772067996cc8a7c335fa95b63eb46919e
Cristian <inesherald@gmail.com>
2019-08-29 17:53:25 -0500

    fix linterns

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class Comment < ApplicationRecord
   validates :comment, presence: true
-  belongs_to :user , foreign_key: "user_id"
-  belongs_to :post ,foreign_key: "post_id"
+  belongs_to :user, foreign_key: 'user_id'
+  belongs_to :post, foreign_key: 'post_id'
 end
--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,23 +1,25 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 class PostTest < ActiveSupport::TestCase
-                       # test "the truth" do
-                       #   assert true
-                       # end
+  # test "the truth" do
+  #   assert true
+  # end
   def setup
-    @post = Post.new(title: "El post que te gusta",body: "Se que te gusta Codear !!")
+    @post = Post.new(title: 'El post que te gusta', body: 'Se que te gusta Codear !!')
   end
-  test "title should be unique" do
+  test 'title should be unique' do
     duplicated_post = @post.dup
     duplicated_post.title = @post.title
     @post.save
     assert_not duplicated_post.valid?
   end
-  test "title should not be blank" do
-    @post.title = " "
+  test 'title should not be blank' do
+    @post.title = ' '
     assert_not @post.valid?
   end
-  test "Body should not be blank" do
-    @post.body = " "
+  test 'Body should not be blank' do
+    @post.body = ' '
     assert_not @post.valid?
   end
 end
