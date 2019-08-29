3ccb71bb57ad411f74c65e8e28500c314430f37e
serdg0 <sergio.diazg92@gmail.com>
2019-08-29 17:44:52 -0500

    fixed linterns

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
-  validates :name, presence: true, length: {minimum: 4, maximum: 16},
-            uniqueness: true
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, uniqueness: true, length: {maximum:  255},
-            format: {with: VALID_EMAIL_REGEX}
+  validates :name, presence: true, length: { minimum: 4, maximum: 16 },
+                   uniqueness: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+  validates :email, presence: true, uniqueness: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX }
   has_secure_password
 end
